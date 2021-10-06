f92c75edd9ef3124b1d76f3f197301d6afd1fa72
Tin La <tinla14@gmail.com>
2021-10-06 14:29:59 -0700

    Fixed client

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import "tailwindcss/tailwind.css";
 import "../../styles/globals.css";
 
-axios.defaults.baseURL = "http://localhost:8000/";
+axios.defaults.baseURL = process.env.REACT_APP_SERVER_API;
 
 function MyApp({ Component, pageProps }) {
   return (
