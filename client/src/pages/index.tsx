0f7ba28f457a8af81b79d427b0749ee2dd467113
Tin La <tinla14@gmail.com>
2021-10-05 20:50:57 -0700

    Added client build

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="my-4 text-3xl font-medium">Got a file? Share It Like Fake News!</h1>
+      <div className="w-96 flex flex-col items-cente bg-gray-800 shadow-xl w-96 rounded-xl justify-center">
+        dropzone
+        render file
+      </div>
+    </div>
+  );
+}
